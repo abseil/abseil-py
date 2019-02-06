905cf0dde6634cdeebed883c6f4087aa59e3023c
Karol M. Langner <langner@google.com>
2019-02-06 15:52:54 -0800

    Add a mark_bool_flags_as_mutual_exclusive convenience function
    PiperOrigin-RevId: 232764112

--- absl/flags/_validators.py
+++ absl/flags/_validators.py
@@ -24,6 +24,7 @@ Three convenience functions are also provided for common flag constraints:
     flags.mark_flag_as_required
     flags.mark_flags_as_required
     flags.mark_flags_as_mutual_exclusive
+    flags.mark_bool_flags_as_mutual_exclusive
 
 See their docstring in this module for a usage manual.
 
@@ -418,6 +419,35 @@ def mark_flags_as_mutual_exclusive(flag_names, required=False,
 
   register_multi_flags_validator(
       flag_names, validate_mutual_exclusion, flag_values=flag_values)
+
+
+def mark_bool_flags_as_mutual_exclusive(flag_names, required=False,
+                                        flag_values=_flagvalues.FLAGS):
+  """Ensures that only one flag among flag_names is True.
+
+  Args:
+    flag_names: [str], names of the flags.
+    required: bool. If true, exactly one flag must be True. Otherwise, at most
+        one flag can be True, and it is valid for all flags to be False.
+    flag_values: flags.FlagValues, optional FlagValues instance where the flags
+        are defined.
+  """
+  for flag_name in flag_names:
+    if not flag_values[flag_name].boolean:
+      raise _exceptions.ValidationError(
+          'Flag --{} is not Boolean, which is required for flags used in '
+          'mark_bool_flags_as_mutual_exclusive.'.format(flag_name))
+
+  def validate_boolean_mutual_exclusion(flags_dict):
+    flag_count = sum(bool(val) for val in flags_dict.values())
+    if flag_count == 1 or (not required and flag_count == 0):
+      return True
+    raise _exceptions.ValidationError(
+        '{} one of ({}) must be True.'.format(
+            'Exactly' if required else 'At most', ', '.join(flag_names)))
+
+  register_multi_flags_validator(
+      flag_names, validate_boolean_mutual_exclusion, flag_values=flag_values)
 
 
 def _add_validator(fv, validator_instance):
