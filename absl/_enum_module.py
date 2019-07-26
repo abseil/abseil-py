42c4a4d609a75e334eeff61f1833aa4fff170f18
Yilei Yang <yileiyang@google.com>
2019-07-26 09:53:05 -0700

    Add license headers.
    PiperOrigin-RevId: 260161648
    Change-Id: I1335bfc9883cc64f8a47d5563ee326e870020965

--- absl/_enum_module.py
+++ absl/_enum_module.py
@@ -1,3 +1,17 @@
+# Copyright 2019 The Abseil Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Import workaround so that Bazel, Py2/Py3, and enum34 package work together.
 
 This works around a problem due to the combination of Bazel putting
