28b57668dbc08573e36c77d4066414d9440cd571
Abseil Team <absl-team@google.com>
2021-03-01 09:15:54 -0800

    Move type comment for `enter_context` to before docstring
    Otherwise, typed_ast will consider it misplaced and fail to parse the module
    
    PiperOrigin-RevId: 360204546
    Change-Id: Ic15ac1173f101c2eb06b078d471d820b16f3bf2b

--- absl/testing/absltest.py
+++ absl/testing/absltest.py
@@ -696,6 +696,7 @@ class TestCase(unittest3_backport.TestCase):
     return tf
 
   def enter_context(self, manager):
+    # type: (ContextManager[_T]) -> _T
     """Returns the CM's value after registering it with the exit stack.
 
     Entering a context pushes it onto a stack of contexts. The context is exited
@@ -714,7 +715,6 @@ class TestCase(unittest3_backport.TestCase):
     Args:
       manager: The context manager to enter.
     """
-    # type: (ContextManager[_T]) -> _T
     if not self._exit_stack:
       raise AssertionError(
           'self._exit_stack is not set: enter_context is Py3-only; also make '
