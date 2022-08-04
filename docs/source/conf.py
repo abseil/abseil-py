6074c1549b898ddaa7e6911eedf3a0534f179d97
byoshimi-github <byoshimi@google.com>
2022-08-04 16:26:23 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,4 +40,4 @@ epub_show_urls = 'footnote'
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../absl'))
