945f9d91ac3ba06278d9184c03a0c88b9ef8d27d
byoshimi-github <byoshimi@google.com>
2022-08-05 09:14:33 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,4 +52,5 @@ epub_show_urls = 'footnote'
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../absl'))
+sys.path.insert(0, os.path.abspath('../..'))       # access to README.md
+sys.path.insert(0, os.path.abspath('../../absl'))  # access to python source code
