5cd577192fb0da1350f8644923bd6bb928e5139b
Amer Elsheikh <amere@google.com>
2026-03-27 10:04:28 -0700

    Remove pyrefly suppressions that are not needed anymore.
    Issue 2641 got fixed and merged to G3
    
    PiperOrigin-RevId: 890511463

--- absl/flags/_argument_parser.py
+++ absl/flags/_argument_parser.py
@@ -147,10 +147,8 @@ class NumericParser(ArgumentParser[_N]):
   def is_outside_bounds(self, val: _N) -> bool:
     """Returns whether the value is outside the bounds or not."""
     return (
-        # pyrefly: ignore[unsupported-operation]  # pyrefly#2641
         (self.lower_bound is not None and val < self.lower_bound)
         or
-        # pyrefly: ignore[unsupported-operation]  # pyrefly#2641
         (self.upper_bound is not None and val > self.upper_bound)
     )
 
